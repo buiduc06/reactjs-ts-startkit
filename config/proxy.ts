0b7220bfb15ee8c12acba7eadf5e4234140698a9
duc.bui <duc.bui@ntq-solution.com.vn>
2022-07-18 19:04:29 +0700

    ci: :wrench: update config

--- config/proxy.ts
+++ config/proxy.ts
@@ -6,12 +6,14 @@
  * For details, please see
  * https://pro.ant.design/docs/deploy
  */
+const { API_ENDPOINT } = process.env;
+
 export default {
   dev: {
     // localhost:8000/api/** -> https://preview.pro.ant.design/api/**
     '/api/': {
       // 要代理的地址
-      target: 'https://preview.pro.ant.design',
+      target: API_ENDPOINT || 'https://preview.pro.ant.design',
       // 配置了这个可以从 http 代理到 https
       // 依赖 origin 的功能可能需要这个，比如 cookie
       changeOrigin: true,
